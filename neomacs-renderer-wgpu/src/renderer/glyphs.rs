35bbc7f60d1ef4ef27a2f0df2e3e7963859fc4ee
Eval Exec <execvy@gmail.com>
2026-03-03 21:45:25 +0800

    renderer: drop stale glyphs module imports

--- neomacs-renderer-wgpu/src/renderer/glyphs.rs
+++ neomacs-renderer-wgpu/src/renderer/glyphs.rs
@@ -2,19 +2,12 @@
 
 use super::super::glyph_atlas::{CachedGlyph, ComposedGlyphKey, GlyphKey, WgpuGlyphAtlas};
 use super::super::vertex::{GlyphVertex, RectVertex, RoundedRectVertex, Uniforms};
+use super::ModeLineFadeEntry;
 use super::WgpuRenderer;
-use super::{
-    ClickHaloEntry, CursorGhostEntry, CursorParticle, EdgeGlowEntry, EdgeSnapEntry, HeatMapEntry,
-    LineAnimEntry, MatrixColumn, ModeLineFadeEntry, RainDrop, RippleWaveEntry, ScrollMomentumEntry,
-    ScrollSpacingEntry, ScrollVelocityFadeEntry, SonarPingEntry, SparkleBurstEntry, TextFadeEntry,
-    TitleFadeEntry, WindowFadeEntry,
-};
 use cosmic_text::SubpixelBin;
 use neomacs_display_protocol::face::{BoxType, Face, FaceAttributes};
-use neomacs_display_protocol::frame_glyphs::{
-    CursorStyle, FrameGlyph, FrameGlyphBuffer, StipplePattern,
-};
-use neomacs_display_protocol::types::{AnimatedCursor, Color, Rect};
+use neomacs_display_protocol::frame_glyphs::{CursorStyle, FrameGlyph, FrameGlyphBuffer};
+use neomacs_display_protocol::types::{AnimatedCursor, Color};
 use std::collections::HashMap;
 use wgpu::util::DeviceExt;
 
