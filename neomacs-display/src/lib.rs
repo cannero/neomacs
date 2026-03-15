c7a19982a6a9318034a469b06f2ce91848190b4b
Eval Exec <execvy@gmail.com>
2026-03-15 14:39:42 +0800

    Delete unused neomacs-display crate

--- neomacs-display/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Neomacs display facade.
-//!
-//! The pure Rust runtime lives in `neomacs-display-runtime`. This crate now
-//! exists only as a temporary Rust-side re-export layer.
-
-#![allow(unused)]
-#![allow(unsafe_op_in_unsafe_fn)]
-
-pub use neomacs_display_runtime::*;
