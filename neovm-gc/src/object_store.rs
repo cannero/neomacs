7cc3ad5241fb9c0384900ff1f465f1330b2b111b
Eval Exec <execvy@gmail.com>
2026-04-15 07:40:58 -0400

    neovm-gc: enlarge object store publish chunks

--- neovm-gc/src/object_store.rs
+++ neovm-gc/src/object_store.rs
@@ -12,7 +12,7 @@ use crate::index_state::{
 use crate::object::{ObjectHeader, ObjectMemoryKind, ObjectRecord};
 
 pub(crate) const OBJECT_STORE_SHARDS: usize = 32;
-const OBJECT_STORE_CHUNK_CAPACITY: usize = 512;
+const OBJECT_STORE_CHUNK_CAPACITY: usize = 768;
 const OBJECT_STORE_SHARD_MASK: usize = OBJECT_STORE_SHARDS - 1;
 
 #[derive(Debug)]
